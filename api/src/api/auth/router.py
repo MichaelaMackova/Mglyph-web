ab03cc3841c66663fba655b75491ecffab960621
Michaela Macková <michaela.mackovaa@gmail.com>
2026-03-06 20:28:02 +0100

    api - automatic user creation after google login

--- api/src/api/auth/router.py
+++ api/src/api/auth/router.py
@@ -17,33 +17,39 @@ router = APIRouter(
 )
 
 @router.post("/google/")
-async def google_auth(credential: CredentialDTO, user_repo: UserRepositoryDep, auth_service: AuthServiceDep) -> LoggedInUserDTO:
+async def google_auth(credential: CredentialDTO, user_repo: UserRepositoryDep, auth_service: AuthServiceDep, user_service: UserServiceDep) -> LoggedInUserDTO:
     try:
         id_info = auth_service.get_google_user_info(credential)
     except mglyph_errors.UnauthorizedError as e:
         raise mglyph_errors.UnauthorizedError.HTTPException(e)
     db_user = await user_repo.get_user_by_google_sub(id_info['sub'])
     if not db_user:
-        err = mglyph_errors.UnauthorizedError("Google user does not have an account.", mglyph_errors.ErrorCode.UNAUTHORIZED_USER_NOT_FOUND)
-        raise mglyph_errors.UnauthorizedError.HTTPException(err)
+        # Create new user
+        user_create = UserCreateDTO(
+            username=id_info['email'], # NOTE: username is set to email by default, possible extension: allow user to specify username
+            email=id_info['email'],
+            google_sub=id_info['sub']
+        )
+        db_user = await user_service.create_user(user_create)
     user_info = UserPublicSimpleDTO.from_model(db_user)
     return LoggedInUserDTO.init_with_new_tokens(user_info)
     
 
-@router.post("/google/create-user/")
-async def google_auth_create_user(google_user: GoogleUserCreateDTO, auth_service: AuthServiceDep, user_service: UserServiceDep) -> LoggedInUserDTO:
-    try:
-        id_info = auth_service.get_google_user_info(google_user.credential)
-    except mglyph_errors.UnauthorizedError as e:
-        raise mglyph_errors.UnauthorizedError.HTTPException(e)
-    # Create new user
-    user_create = UserCreateDTO(
-        username=google_user.username,
-        email=id_info['email'],
-        google_sub=id_info['sub']
-    )
-    db_user = await user_service.create_user(user_create)
-    return LoggedInUserDTO.init_with_new_tokens(UserPublicSimpleDTO.from_model(db_user))
+# NOTE: User creation is automatic when a user logs in with Google for the first time. Possible user creation:
+# @router.post("/google/create-user/")
+# async def google_auth_create_user(google_user: GoogleUserCreateDTO, auth_service: AuthServiceDep, user_service: UserServiceDep) -> LoggedInUserDTO:
+#     try:
+#         id_info = auth_service.get_google_user_info(google_user.credential)
+#     except mglyph_errors.UnauthorizedError as e:
+#         raise mglyph_errors.UnauthorizedError.HTTPException(e)
+#     # Create new user
+#     user_create = UserCreateDTO(
+#         username=google_user.username,
+#         email=id_info['email'],
+#         google_sub=id_info['sub']
+#     )
+#     db_user = await user_service.create_user(user_create)
+#     return LoggedInUserDTO.init_with_new_tokens(UserPublicSimpleDTO.from_model(db_user))
 
 
 
