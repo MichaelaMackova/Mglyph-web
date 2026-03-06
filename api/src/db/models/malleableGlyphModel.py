59ee71ff98e83a8f320a8b5cde26f26b50d133ae
Michaela Macková <michaela.mackovaa@gmail.com>
2026-03-06 22:28:54 +0100

    api & migrations - add mglyph and report_flag relationship

--- api/src/db/models/malleableGlyphModel.py
+++ api/src/db/models/malleableGlyphModel.py
@@ -6,6 +6,7 @@ from typing import Optional
 
 import db.models.userModel as userModel
 import db.models.mglyphEvaluationModel as mglyphEvaluationModel
+import db.models.mglyphReportFlagModel as mglyphReportFlagModel
 
 class MalleableGlyphModel(SQLModel, table=True):
     __tablename__ = "malleable_glyph"
@@ -25,3 +26,4 @@ class MalleableGlyphModel(SQLModel, table=True):
     # Relationships
     creator: "userModel.UserModel" = Relationship(back_populates="malleable_glyphs", sa_relationship_kwargs=dict(lazy='raise_on_sql'))
     mglyph_evaluation_links: list["mglyphEvaluationModel.MGlyphEvaluationModel"] = Relationship(back_populates="malleable_glyph", sa_relationship_kwargs=dict(lazy='raise_on_sql'))
+    report_flags: list["mglyphReportFlagModel.MGlyphReportFlagModel"] = Relationship(back_populates="mglyph", sa_relationship_kwargs=dict(lazy='raise_on_sql'))
--- api/src/db/models/mglyphReportFlagModel.py
+++ api/src/db/models/mglyphReportFlagModel.py
@@ -6,6 +6,7 @@ from typing import Optional
 from enum import Enum as PyEnum
 
 import db.models.userModel as userModel
+import db.models.malleableGlyphModel as malleableGlyphModel
 
 
 
@@ -29,9 +30,11 @@ class MGlyphReportFlagModel(SQLModel, table=True):
     last_updated_time: datetime | None = Field(default=None, sa_type=DateTime(timezone=True))
     history: str = Field() # A string that keeps track of the history of the flag, e.g. when it was created, when it was closed, etc.
 
+    mglyph_id: UUID = Field(index=True, foreign_key="malleable_glyph.id")
     user_reporter_id: UUID = Field(foreign_key="end_user.id")
     user_resolver_id: Optional[UUID] = Field(default=None, foreign_key="end_user.id")
 
     # Relationships
+    mglyph: "malleableGlyphModel.MalleableGlyphModel" = Relationship(back_populates="report_flags", sa_relationship_kwargs=dict(lazy='raise_on_sql'))
     user_reporter: "userModel.UserModel" = Relationship(back_populates="reported_flags", sa_relationship_kwargs=dict(foreign_keys="MGlyphReportFlagModel.user_reporter_id", lazy='raise_on_sql'))
     user_resolver: Optional["userModel.UserModel"] = Relationship(back_populates="resolved_flags", sa_relationship_kwargs=dict(foreign_keys="MGlyphReportFlagModel.user_resolver_id", lazy='raise_on_sql'))
